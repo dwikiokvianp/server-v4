72ca34d4d317a4fbdb0bfbca43ffad8c59660c96
ilham <aptailham6@gmail.com>
2023-06-15 17:12:55 +0700

    update list today

--- models/Company.go
+++ models/Company.go
@@ -2,8 +2,8 @@ package models
 
 type Company struct {
 	Id             int    `json:"id" gorm:"primaryKey;autoIncrement"`
-	CompanyName    string `json:"username"`
-	Address        string `json:"password"`
+	CompanyName    string `json:"companyName"`
+	Address        string `json:"Address"`
 	CompanyDetail  string `json:"company_detail"`
 	CompanyZipCode int    `json:"company_zip_code"`
 	CreatedAt      int64  `gorm:"autoCreateTime" json:"created_at"`
