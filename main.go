3478e11f2f54cbf5af9de2d8e7897b532af303bd
dwikiokvianpradana <dwikiokvianp1999@gmailcom>
2023-07-12 00:16:35 +0700

    feat - add /ping endpoint to test github action

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 	server.Use(cors.New(myCorsConfig))
 	server.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello mantap mantap from github actions",
+			"message": "Github action test",
 		})
 	})
 	routes.Routes(server)
